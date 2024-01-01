5805e7a6950b2181bd1af2d4827afaaa2b2eaf73
Ne'kko Montoya <87720386+nekMont@users.noreply.github.com>
2024-01-01 16:07:55 -0700

    added sample heat map from documentation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <CalendarHeatmap
+          className="calendar-heatmap"
+          startDate={new Date("2024-01-01")}
+          endDate={new Date("2024-12-31")}
+          values={[
+            { date: "2024-01-01", count: 12 },
+            // ...and so on
+          ]}
+        />
+      </div>
     </div>
   );
 }
